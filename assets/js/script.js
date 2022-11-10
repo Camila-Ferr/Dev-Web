36ec9c4fe250039144d1b4e5b90b6824b011cd3c
Camila-Ferr <cf_alves@id.uff.br>
2022-11-09 22:13:04 -0300

    finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,67 @@
 //Para likes
-$(`.like-btn`).each(function() {
-   
+$(`.like-btn`).on("click", function(){
    let dataLike = $(this).parent().find('span:first').data("like")
    let dataDeslike = $(this).parent().find('span:eq(1)').data("dislike")
 
+   if ($(this).children().hasClass("far fa-thumbs-up")) {
 
-   $(this).on("click", function(){
-
-      if ($(this).children().hasClass("far fa-thumbs-up")) {
-
-         //Acrescenta mais um   
-         $(this).parent().find('span:first').text(dataLike + 1)
+      //Acrescenta mais um   
+      $(this).parent().find('span:first').text(dataLike + 1)
 
                
-         //Remove uma classe e adiciona outra  
-         $(this).children().removeClass("far fa-thumbs-up");
-         $(this).children().addClass("fas fa-thumbs-up");
+      //Remove uma classe e adiciona outra  
+      $(this).children().removeClass("far fa-thumbs-up");
+      $(this).children().addClass("fas fa-thumbs-up");
 
-         //Se o ícone de dislike estiver clicado
-         if($(this).next().children().hasClass("fas fa-thumbs-down")){
+      //Se o ícone de dislike estiver clicado
+      if($(this).next().children().hasClass("fas fa-thumbs-down")){
 
-            $(this).parent().find('span:eq(1)').text(dataDeslike);
-            $(this).next().children().removeClass("fas fa-thumbs-down");
-            $(this).next().children().addClass("far fa-thumbs-down");
-         }
+         $(this).parent().find('span:eq(1)').text(dataDeslike);
+         $(this).next().children().removeClass("fas fa-thumbs-down");
+         $(this).next().children().addClass("far fa-thumbs-down");
       }
+   }
       
-      //Se o ícone estiver clicado e clicar novamente, desmarca
-      else if ($(this).children().hasClass("fas fa-thumbs-up")){ //Botão oposto
+   //Se o ícone estiver clicado e clicar novamente, desmarca
+   else if ($(this).children().hasClass("fas fa-thumbs-up")){ //Botão oposto
 
-         $(this).parent().find('span:first').text(dataLike);
-         $(this).children().removeClass("fas fa-thumbs-up");
-         $(this).children().addClass("far fa-thumbs-up");
-      }
-   })
+      $(this).parent().find('span:first').text(dataLike);
+      $(this).children().removeClass("fas fa-thumbs-up");
+      $(this).children().addClass("far fa-thumbs-up");
+   }
 });
       
 //Para dislikes
-$(`.dislike-btn`).each(function() {
+$(`.dislike-btn`).on("click", function(){
+      
+   let dataLike = $(this).parent().find('span:first').data("like")
+   let dataDeslike = $(this).parent().find('span:eq(1)').data("dislike")
+      
+   //Se não está marcado
+   if ($(this).children().hasClass("far fa-thumbs-down")) { //Botão está clicado
+         
+      //Acrescenta mais um
+      $(this).parent().find('span:eq(1)').text(dataDeslike + 1);
+         
+      //Remove uma classe e adiciona outra  
+      $(this).children().removeClass("far fa-thumbs-down");
+      $(this).children().addClass("fas fa-thumbs-down");
 
-   $(this).on("click", function(){
-      
-      let id_like = $(this).prev().prev().attr('id')
-      let dataLike = $(this).parent().find('span:first').data("like")
-   
-      let dataDeslike = $(this).parent().find('span:eq(1)').data("dislike")
-      
-      //Se não está marcado
-      if ($(this).children().hasClass("far fa-thumbs-down")) { //Botão está clicado
+      if ($(this).prev().children().hasClass("fas fa-thumbs-up")){ //Se o like está clicado
          
-         //Acrescenta mais um
-         $(this).parent().find('span:eq(1)').text(dataDeslike + 1);
-         
-         //Remove uma classe e adiciona outra  
-         $(this).children().removeClass("far fa-thumbs-down");
-         $(this).children().addClass("fas fa-thumbs-down");
-
-         if ($(this).prev().children().hasClass("fas fa-thumbs-up")){ //Se o like está clicado
-         
-            $(this).parent().find('span:first').text(dataLike);
-            $(this).prev().children().removeClass("fas fa-thumbs-up");
-            $(this).prev().children().addClass("far fa-thumbs-up");
-         }
+         $(this).parent().find('span:first').text(dataLike);
+         $(this).prev().children().removeClass("fas fa-thumbs-up");
+         $(this).prev().children().addClass("far fa-thumbs-up");
       }
+   }
       
-      //Se está marcado
-      else if ($(this).children().hasClass("fas fa-thumbs-down")){ //Se o dislike está clicado, desclica
+   //Se está marcado
+   else if ($(this).children().hasClass("fas fa-thumbs-down")){ //Se o dislike está clicado, desclica
          
-         $(this).parent().find('span:eq(1)').text(dataDeslike);
-         $(this).children().removeClass("fas fa-thumbs-down");
-         $(this).children().addClass("far fa-thumbs-down");
-      }
-   })
+      $(this).parent().find('span:eq(1)').text(dataDeslike);
+      $(this).children().removeClass("fas fa-thumbs-down");
+      $(this).children().addClass("far fa-thumbs-down");
+   }
 });
     
 
